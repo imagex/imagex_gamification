ba48f7c52cf80f159b45fd453e9b5f848d1dc046
Adam Gregory <arcaneadam@gmail.com>
2014-01-06 00:08:32 -0800

    Adding file for push

---
+++ modules/imagex_userbadge/imagex_userbadge.tpl.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * User Badge Template
+ */
+?>
+<?php print kpr(get_defined_vars()); ?>
+<div id="user-badge-<?php print $imagex_userbadge->badge_id;?>"  class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+  
+  <?php print render($title_prefix); ?>
+  <?php if (!$page): ?>
+    <h2<?php print $title_attributes; ?>><a href="<?php print $url; ?>"><?php print $title; ?></a></h2>
+  <?php endif; ?>
+  <?php print render($title_suffix); ?>
+  
+  <div class="content"<?php print $content_attributes; ?>>
+    <?php print render($content);?>
+  </div>
+</div>
